984a8ac41a6b3d53f4f5ef508da025c3dec8fd8b
babken0 <babkenboyaxchyan@gmail.com>
2020-08-21 00:47:09 +0400

    push

---
+++ src/listTask/Main.java
@@ -0,0 +1,28 @@
+package listTask;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ArrayList<String> s = new ArrayList<>();
+        CustomArrayList<String> customArrayList = new CustomArrayList<>();
+
+        customArrayList.add("C");
+        customArrayList.add("Java");
+        customArrayList.add("C++");
+
+//
+        System.out.println(customArrayList.toString());
+
+
+
+
+
+
+    }
+}
