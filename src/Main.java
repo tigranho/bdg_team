e752e8baf0e8f1cf61da5fb271d147ba2fdd6fbc
Samvelll <samvelkhach1@mail.ru>
2020-08-21 14:59:27 +0400

    CustomArrayList and CustomLinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import sava.util.CustomArrayList;
+import sava.util.CustomLinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Integer> list1 = new CustomArrayList<>();
+        List<String> list2 = new CustomLinkedList<>();
+
+        list2.add("Samvel");
+        list2.add("Armen");
+        list2.add("Sargis");
+
+        System.out.println(list2);
+
+        list1.add(8);
+        list1.add(9);
+        list1.add(4);
+        list1.add(9);
+
+        list1.add(3);
+
+        System.out.println(list1);
     }
 }
