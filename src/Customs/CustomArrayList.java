946870a6df7d461c4159db04adc600364f27e627
Ed <edgar.ghazaryan888@gmail.com>
2020-08-21 14:06:21 +0400

    Adding CustomArrayList and CustomLinkedList

--- src/Customs/CustomArrayList.java
+++ src/Customs/CustomArrayList.java
@@ -1,4 +1,313 @@
 package Customs;
 
-public class CustomArrayList {
+import java.util.*;
+
+public class CustomArrayList<E> implements List<E> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private static final Object[] EMPTY_DATA = {};
+
+    private Object[] data;
+
+    private int size;
+
+    private int currentCapacity;
+
+    public CustomArrayList() {
+        data = new Object[DEFAULT_CAPACITY];
+        size = 0;
+        currentCapacity = DEFAULT_CAPACITY;
+    }
+
+    public CustomArrayList(int capacity) {
+        if(capacity < 0) {
+            throw new IllegalArgumentException("Illegal capacity" + capacity);
+        }
+        size = 0;
+        currentCapacity = capacity;
+        if(capacity > 0) {
+            data = new Object[capacity];
+        }
+        else {
+            data = EMPTY_DATA;
+        }
+    }
+
+    public CustomArrayList(Collection<? extends E> c) {
+        data = c.toArray();
+        size = data.length;
+        if (data.getClass() != Object[].class) {
+            data = Arrays.copyOf(data, size, Object[].class);
+        }
+    }
+
+    public void trimToSize() {
+        if(size < data.length) {
+            currentCapacity = size;
+            data = (size == 0) ? EMPTY_DATA : Arrays.copyOf(data, size);
+        }
+    }
+
+    public void ensureCapacity(int capacity) {
+        if(capacity > size) {
+            data = Arrays.copyOf(data, capacity);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("[ ");
+        for(int i = 0; i < size; ++i) {
+            str.append(data[i] + " ");
+        }
+        str.append("]");
+        return str.toString();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    // To do
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(data, size);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            return (T[]) Arrays.copyOf(data, size, a.getClass());
+        }
+        System.arraycopy(data, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if(size == currentCapacity) {
+            ensureCapacity(currentCapacity *= 2);
+        }
+        data[size++] = e;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) {
+            for(int index = 0; index < size; ++index) {
+                if(data[index] == null) {
+                    removeHelp(index);
+                    return true;
+                }
+            }
+        }
+        else {
+            for (int index = 0; index < size; index++)
+                if (o.equals(data[index])) {
+                    removeHelp(index);
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    private void removeHelp(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(data, index + 1, data, index,
+                    numMoved);
+        }
+        data[--size] = null;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for(Object e : c) {
+            if(!this.contains(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        Object[] a = c.toArray();
+        ensureCapacity(size + a.length);
+        System.arraycopy(a, 0, data, size, a.length);
+        size += a.length;
+        return a.length != 0;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
+
+        Object[] a = c.toArray();
+        ensureCapacity(size + a.length);
+        int numMoved = size - index;
+        if (numMoved > 0)
+            System.arraycopy(data, index, data, index + a.length,
+                    numMoved);
+
+        System.arraycopy(a, 0, data, index, a.length);
+        size += a.length;
+        return a.length != 0;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean isRemoved = false;
+        for(Object e : c) {
+            if(this.contains(e)) {
+                this.remove(e);
+                size--;
+                isRemoved = true;
+            }
+        }
+        return isRemoved;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean isRemoved = false;
+        for(Object e : c) {
+            if(!this.contains(e)) {
+                this.remove(e);
+                size--;
+                isRemoved = true;
+            }
+        }
+        return isRemoved;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndex(index);
+
+        return (E) data[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkIndex(index);
+
+        Object old = data[index];
+        data[index] = element;
+        return (E) old;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        checkIndex(index);
+
+        ensureCapacity(size + 1);
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = element;
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+
+        E old = (E) data[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(data, index + 1, data, index, numMoved);
+        }
+        data[--size] = null;
+        return old;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (data[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(data[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = size-1; i >= 0; i--) {
+                if (data[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = size-1; i >= 0; i--) {
+                if (o.equals(data[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //To do
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    //To do
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    //To do
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
+    }
+
 }
--- src/Customs/CustomLinkedList.java
+++ src/Customs/CustomLinkedList.java
@@ -1,4 +1,501 @@
 package Customs;
 
-public class CustomLinkedList {
+import java.util.*;
+
+public class CustomLinkedList<E> implements List<E>, Deque<E> {
+
+    private Node<E> first;
+
+    private Node<E> last;
+
+    private int size;
+
+    public CustomLinkedList() {}
+
+    public CustomLinkedList(Collection<? extends E> c) {
+        addAll(c);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("[ ");
+        for(Node<E> x = first; x != null; x = x.next) {
+            str.append(x.item + " ");
+        }
+        str.append("]");
+        return str.toString();
+    }
+
+    @Override
+    public void addFirst(E e) {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null) {
+            last = newNode;
+        }
+        else {
+            f.prev = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void addLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if(l == null) {
+            first = newNode;
+        }
+        else {
+            l.next = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean offerFirst(E e) {
+        addFirst(e);
+        return true;
+    }
+
+    @Override
+    public boolean offerLast(E e) {
+        addLast(e);
+        return true;
+    }
+
+    @Override
+    public E removeFirst() {
+        if(first == null) {
+            throw new NoSuchElementException("The list is empty.");
+        }
+        final Node<E> f = first;
+        return unlink(f);
+    }
+
+    @Override
+    public E removeLast() {
+        if(last == null) {
+            throw new NoSuchElementException("The list is empty.");
+        }
+        final Node<E> l = last;
+        return unlink(l);
+    }
+
+    @Override
+    public E pollFirst() {
+        return (first == null) ? null : removeFirst();
+    }
+
+    @Override
+    public E pollLast() {
+        return (last == null) ? null : removeLast();
+    }
+
+    @Override
+    public E getFirst() {
+        if(first == null) {
+            throw new NoSuchElementException("The list is empty.");
+        }
+        final E element = first.item;
+        return element;
+    }
+
+    @Override
+    public E getLast() {
+        if(last == null) {
+            throw new NoSuchElementException("The list is empty.");
+        }
+        final E element = last.item;
+        return element;
+    }
+
+    @Override
+    public E peekFirst() {
+        final E element = first.item;
+        return (first == null) ? null : element;
+    }
+
+    @Override
+    public E peekLast() {
+        final E element = last.item;
+        return (last == null) ? null : element;
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        return remove(o);
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        if (o == null) {
+            for (Node<E> x = last; x != null; x = x.prev) {
+                if (x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> x = last; x != null; x = x.prev) {
+                if (o.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return add(e);
+    }
+
+    @Override
+    public E remove() {
+        return removeFirst();
+    }
+
+    @Override
+    public E poll() {
+        return (first == null) ? null : removeFirst();
+    }
+
+    @Override
+    public E element() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        final E element = first.item;
+        return element;
+    }
+
+    @Override
+    public E peek() {
+        final E element = first.item;
+        return (first == null) ? null : element;
+    }
+
+    @Override
+    public void push(E e) {
+        addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return removeFirst();
+    }
+
+    //To do
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) != -1;
+    }
+
+    //To do
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] arr = new Object[size];
+        int i = 0;
+        for(Node<E> x = first; x != null; x = x.next) {
+            arr[i++] = x.item;
+        }
+        return arr;
+    }
+
+    //To do
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        addLast(e);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if(o == null) {
+            for(Node<E> x = first; x != null; x = x.next) {
+                if(x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        else {
+            for(Node<E> x = first; x != null; x = x.next) {
+                if(o.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for(Object e : c) {
+            if(!this.contains(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        if(c.size() == 0) {
+            return false;
+        }
+        for(E e : c) {
+            addLast(e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        if(c.size() == 0) {
+            return false;
+        }
+        for(E e : c) {
+            add(index++, e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean isRemoved = false;
+        for(Object e : c) {
+            if(contains(e)) {
+                remove(e);
+                isRemoved = true;
+            }
+        }
+        return isRemoved;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean isRemoved = false;
+        for(Object e : c) {
+            if(!contains(e)) {
+                remove(e);
+                isRemoved = true;
+            }
+        }
+        return isRemoved;
+    }
+
+    @Override
+    public void clear() {
+        for(Node<E> x = first; x != null; ) {
+            Node<E> next = x.next;
+            x.item = null;
+            x.prev = null;
+            x.next = null;
+            x = next;
+        }
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    @Override
+    public E get(int index) {
+        if(first == null) {
+            throw new NoSuchElementException("The list is empty.");
+        }
+        checkIndex(index);
+
+        Node<E> current = first;
+        while(index-- != 0) {
+            current = current.next;
+        }
+        final Node<E> result = current;
+        return result.item;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        if(first == null) {
+            throw new NoSuchElementException("The list is empty.");
+        }
+        checkIndex(index);
+
+        Node<E> current = first;
+        while(index-- != 0) {
+            current = current.next;
+        }
+        final E result = current.item;
+        current.item = element;
+        return result;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        checkIndex(index);
+
+        Node<E> current = first;
+        while(index-- != 0) {
+            current = current.next;
+        }
+
+        if(current.prev == null) {
+            addFirst(element);
+        }
+        else if(current.next == null) {
+            addLast(element);
+        }
+        else {
+            Node<E> newNode = new Node<E>(current.prev, element, current);
+            current.prev = newNode;
+            current.prev.next = newNode;
+        }
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+
+        Node<E> current = first;
+        while(index-- != 0) {
+            current = current.next;
+        }
+        final E element = current.item;
+        unlink(current);
+        return element;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        int index = 0;
+        if(o == null) {
+            for(Node<E> x = first; x != null; x = x.next) {
+                if(x.item == null) {
+                    return index;
+                }
+                index++;
+            }
+        }
+        else {
+            for(Node<E> x = first; x != null; x = x.next) {
+                if(o.equals(x.item)) {
+                    return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        int index = size;
+        if(o == null) {
+            for(Node<E> x = last; x != null; x = x.prev) {
+                if(x.item == null) {
+                    return index;
+                }
+                index--;
+            }
+        }
+        else {
+            for(Node<E> x = last; x != null; x = x.prev) {
+                if(o.equals(x.item)) {
+                    return index;
+                }
+                index--;
+            }
+        }
+        return -1;
+    }
+
+    //To do
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    //To do
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    //To do
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+    private void checkIndex(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds.");
+        }
+    }
+
+    private E unlink(Node<E> toUnlink) {
+        final E element = toUnlink.item;
+        final Node<E> prev = toUnlink.prev;
+        final Node<E> next = toUnlink.next;
+
+        if(prev == null) {
+            first = next;
+        }
+        else {
+            prev.next = next;
+            toUnlink.prev = null;
+        }
+        if(next == null) {
+            last = prev;
+        }
+        else {
+            next.prev = prev;
+            toUnlink.next = null;
+        }
+        toUnlink.item = null;
+        size--;
+        return element;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
 }
