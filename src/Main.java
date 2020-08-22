36e9dd982ce66ead5085064cf5b7cecfa10a42fd
Ed <edgar.ghazaryan888@gmail.com>
2020-08-22 19:20:42 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import Customs.CustomArrayList;
-import Customs.CustomLinkedList;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
