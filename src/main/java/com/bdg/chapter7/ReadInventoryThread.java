2511618c67df257f19ec37651bc29d7daacc553c
Karlen999 <69694626+Karlen999@users.noreply.github.com>
2020-08-31 18:03:16 +0400

    implements Runnable

---
+++ src/main/java/com/bdg/chapter7/ReadInventoryThread.java
@@ -0,0 +1,13 @@
+package com.bdg.chapter7;
+
+public class ReadInventoryThread extends Thread {
+    public void run() {
+        System.out.println("Printing zoo inventory");
+    }
+
+    public static void main(String[] args) {
+        (new ReadInventoryThread()).start();
+    }
+
+
+}
