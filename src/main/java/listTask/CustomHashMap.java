d80649a9b3bc74c14db455630e47830e90e962e4
babken0 <babkenboyaxchyan@gmail.com>
2020-09-13 23:15:06 +0400

    Add HashMap

---
+++ src/main/java/listTask/CustomHashMap.java
@@ -0,0 +1,195 @@
+package listTask;
+
+import java.util.*;
+
+
+public class CustomHashMap<K,V> implements Map<K,V> {
+
+    private static  final int DEFAULT_INITIAL_CAPACITY = 16;
+    private final Node<K,V>[] nodes;
+    private int size = 0;
+
+    public CustomHashMap() {
+        nodes = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        int i = key.hashCode()% nodes.length;
+        Node<K,V> temp = nodes[i];
+        while(temp != null){
+            if (temp.getKey().equals(key)){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for (Node<K,V> node:nodes) {
+            Node<K,V> temp =node;
+            while(temp != null){
+                if (!temp.getValue().equals(value)) {
+                    temp = temp.next;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        int i = key.hashCode()% nodes.length;
+        Node<K,V> temp = nodes[i];
+        while(temp != null){
+            if (temp.getKey().equals(key)){
+                return  temp.getValue();
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        int i = key.hashCode()% nodes.length;
+        if (containsKey(key)) {
+            Node<K,V> temp = nodes[i];
+            while(temp != null){
+                if (temp.getKey().equals(key)){
+                    V val =  temp.getValue();
+                    temp.setValue(value);
+                    return val;
+                }
+                temp = temp.next;
+            }
+        }
+        Node<K,V> temp = nodes[i];
+        while(temp != null){
+            if (temp.next == null){
+                temp.next = new Node<>(key, value);
+                size++;
+                return null;
+            }
+            temp = temp.next;
+        }
+        nodes[i]= new Node<>(key, value);
+        size++;
+
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        V value = null;
+        int i = key.hashCode()% nodes.length;
+        Node<K,V> temp = nodes[i];
+        if (temp.getKey().equals(key) & temp.next == null){
+            value =  temp.getValue();
+            temp.key= null;
+            temp.value = null;
+        }
+        while(temp.next != null){
+            if (temp.next.getKey().equals(key)){
+                value =  temp.next.getValue();
+                temp.next = temp.next.next ;
+            }
+            temp = temp.next;
+        }
+        return value;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+        for (Entry<? extends K, ? extends V> n : m.entrySet()) {
+                put(n.getKey(),n.getValue());
+            }
+    }
+
+
+    @Override
+    public void clear() {
+        Arrays.fill(nodes, null);
+        size = 0;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<>();
+        for(Node<K,V> n : nodes){
+            set.add( n.getKey());
+        }
+
+        return set;
+    }
+
+    @Override
+    public Collection<V> values() {
+        Collection<V> v = new ArrayList<>();
+        for(Node<K,V> n : nodes){
+            v.add( n.getValue());
+        }
+        return v;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return new HashSet<>(Arrays.asList(nodes));
+    }
+
+    private static class Node<K,V> implements Map.Entry<K,V>{
+        int hash;
+        K key;
+        V value;
+        Node<K,V> next ;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+             hash = hashCode();
+        }
+
+
+        public K getKey() {
+            return key;
+        }
+
+
+        public V getValue() {
+            return value;
+        }
+
+
+        public V setValue(V value) {
+            this.value =  value;
+            return null;
+        }
+
+        @Override
+        public int hashCode() {
+            hash=31;
+            hash=hash*17+key.hashCode();
+            hash=hash*17+value.hashCode();
+            return hash;
+        }
+
+
+    }
+
+
+}
--- src/main/java/listTask/Main.java
+++ src/main/java/listTask/Main.java
@@ -6,7 +6,7 @@ import java.util.function.Supplier;
 
 import static java.lang.invoke.MethodHandles.identity;
 import static javafx.scene.input.KeyCode.T;
-
+import static javafx.scene.input.KeyCode.V;
 
 
 public class Main {
@@ -14,11 +14,21 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> s = new ArrayList<>();
-        CustomArrayList<String> customArrayList = new CustomArrayList<>();
+        CustomHashMap<String,String > customHashMaps = new CustomHashMap<>();
+        HashMap<String ,String> hashMap = new HashMap<>();
 
-        customArrayList.add("C");
-        customArrayList.add("Java");
-        customArrayList.add("C++");
+        hashMap.put("Barev","Mal");
+        hashMap.put("Barev10","Mal");
+        hashMap.put("Barev20","Mal");
+
+        customHashMaps.putAll(hashMap);
+//        customHashMaps.put("barev","njdfjnjds");
+//        customHashMaps.put("barev","njdfjnjds");
+//
+//        //System.out.println(customHashMaps.get("Barev"));
+//        System.out.println(customHashMaps.get("barev"));
+
+        System.out.println(customHashMaps.get("Barev"));
 
 
     }
