d08ee31a380f6e8a2ac42c5f9c719d8f7bc4854d
Artur Tomeyan <tomeyan.artur@gmail.com>
2020-09-15 14:24:21 +0400

    Consumer/Producer with API

---
+++ src/main/java/tasks/threads/task3/modyfied/Consumer.java
@@ -0,0 +1,27 @@
+package tasks.threads.task3.modyfied;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable{
+
+    private final BlockingQueue<String> blockingQueue;
+
+    Consumer(BlockingQueue<String> blockingQueue){
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        try{
+            //noinspection InfiniteLoopStatement
+            while(true) {
+
+                String data = blockingQueue.take();
+                System.out.println("Consumed by: " + Thread.currentThread().getName() + " data: " + data);
+                blockingQueue.remove(data);
+            }
+        } catch (InterruptedException ignored) {
+            System.out.println("Consumer thread is interrupted");
+        }
+    }
+}
